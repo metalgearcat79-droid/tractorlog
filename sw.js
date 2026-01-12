2af8dbd60d277b3e89df9e52049dfb077b455ba5
Kaori <metalgearcat.79@gmail.com>
2026-01-12 15:00:27 +0900

    Edge が PWA と認識するための最低限の処理

--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
 self.addEventListener("install", () => {
   self.skipWaiting();
 });
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener("fetch", () => {
+  // Edge が PWA と認識するための最低限の処理
+});
