317d31789e12b76d694098f5c550aa55e7c015aa
Kaori <metalgearcat.79@gmail.com>
2026-01-14 20:49:34 +0900

    修正版 sw.js

--- sw.js
+++ sw.js
@@ -6,6 +6,6 @@ self.addEventListener("activate", (event) => {
   event.waitUntil(clients.claim());
 });
 
-self.addEventListener("fetch", () => {
-  // Edge が PWA と認識するための最低限の処理
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request));
 });
